b8dd6ba3d03d96ff86841783f77e909d3121caee
Gkay <114266116+GkayAIO@users.noreply.github.com>
2023-04-30 18:24:41 +0200

    Create colors.py

---
+++ data/colors.py
@@ -0,0 +1,15 @@
+from sty import Style, RgbFg, fg 
+
+fg.light_yellow = Style(RgbFg(255, 255, 0))
+fg.dark_yellow = Style(RgbFg(190, 199, 0))
+fg.grey = Style(RgbFg(215, 215, 215))
+fg.light_green = Style(RgbFg(0, 255, 111))
+fg.dark_red = Style(RgbFg(188, 37, 44))
+fg.light_red = Style(RgbFg(208, 31, 39))
+fg.pink = Style(RgbFg(255, 0, 232))
+fg.dark_purple = Style(RgbFg(100, 0, 188))
+fg.light_purple = Style(RgbFg(140, 30, 255))
+fg.light_blue = Style(RgbFg(0, 255, 255))
+fg.dark_blue = Style(RgbFg(0, 70, 255))
+fg.light_grey = Style(RgbFg(190, 190, 190))
+fg.dark_grey = Style(RgbFg(110, 110, 110))
